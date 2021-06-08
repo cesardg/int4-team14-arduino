90d41ca1b9b742e5a8af8575a6af0573675d8e93
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-06-08 23:41:29 +0200

    update keycode

--- boardgame-uservshacker/boardgame-uservshacker.ino
+++ boardgame-uservshacker/boardgame-uservshacker.ino
@@ -26,31 +26,31 @@ void loop() {
   else if (analogRead1 < 95) {activateKeyboard(); Keyboard.press('7'); endKeyboard();}
   else if (analogRead1 < 110) {activateKeyboard(); Keyboard.press('8'); endKeyboard();}
   else if (analogRead1 < 120) {activateKeyboard(); Keyboard.press('9'); endKeyboard();}
-  else if (analogRead1 < 140) {activateKeyboard(); Keyboard.print("A"); endKeyboard()}
-  else if (analogRead1 < 170) {activateKeyboard(); Keyboard.print("B"); endKeyboard();}
-  else if (analogRead1 < 200) {activateKeyboard(); Keyboard.print("C"); endKeyboard();}
-  else if (analogRead1 < 250) {activateKeyboard(); Keyboard.print("D"); endKeyboard();}
-  else if (analogRead1 < 350) {activateKeyboard(); Keyboard.print("E"); endKeyboard();}
-  else if (analogRead1 < 600) {activateKeyboard(); Keyboard.print("F"); endKeyboard();}
-  else {activateKeyboard(); Keyboard.print("F"); endKeyboard();}
+  else if (analogRead1 < 140) {activateKeyboard(); Keyboard.print('a'); endKeyboard();}
+  else if (analogRead1 < 170) {activateKeyboard(); Keyboard.print('b'); endKeyboard();}
+  else if (analogRead1 < 200) {activateKeyboard(); Keyboard.print('c'); endKeyboard();}
+  else if (analogRead1 < 250) {activateKeyboard(); Keyboard.print('d'); endKeyboard();}
+  else if (analogRead1 < 350) {activateKeyboard(); Keyboard.print('e'); endKeyboard();}
+  else if (analogRead1 < 600) {activateKeyboard(); Keyboard.print('f'); endKeyboard();}
+  else {activateKeyboard(); Keyboard.print('g'); endKeyboard();}
      
   if (analogRead2 < 25) {/*Serial.println("no button")*/ }
-  else if (analogRead2 < 40) {activateKeyboard(); Keyboard.print('V'); endKeyboard();}
-  else if (analogRead2 < 50) {activateKeyboard(); Keyboard.print('U'); endKeyboard();}
-  else if (analogRead2 < 60) {activateKeyboard(); Keyboard.print('T'); endKeyboard();}
-  else if (analogRead2 < 65) {activateKeyboard(); Keyboard.press('S'); endKeyboard();}
-  else if (analogRead2 < 75) {activateKeyboard(); Keyboard.press('R'); endKeyboard();}
-  else if (analogRead2 < 85) {activateKeyboard(); Keyboard.press('Q'); endKeyboard();}
-  else if (analogRead2 < 95) {activateKeyboard(); Keyboard.press('P'); endKeyboard();}
-  else if (analogRead2 < 110) {activateKeyboard(); Keyboard.press('O'); endKeyboard();}
-  else if (analogRead2 < 120) {activateKeyboard(); Keyboard.press('N'); endKeyboard(); }
-  else if (analogRead2 < 140) {activateKeyboard(); Keyboard.print("M"); endKeyboard()}
-  else if (analogRead2 < 170) {activateKeyboard(); Keyboard.print("L"); endKeyboard();}
-  else if (analogRead2 < 200) {activateKeyboard(); Keyboard.print("K"); endKeyboard();}
-  else if (analogRead2 < 250) {activateKeyboard(); Keyboard.print("J"); endKeyboard();}
-  else if (analogRead2 < 350) {activateKeyboard(); Keyboard.print("I"); endKeyboard();}
-  else if (analogRead2 < 600) {activateKeyboard(); Keyboard.print("H"); endKeyboard();}
-  else {activateKeyboard(); Keyboard.print("G"); endKeyboard();}
+  else if (analogRead2 < 40) {activateKeyboard(); Keyboard.print('z'); endKeyboard();}
+  else if (analogRead2 < 50) {activateKeyboard(); Keyboard.print('y'); endKeyboard();}
+  else if (analogRead2 < 60) {activateKeyboard(); Keyboard.print('w'); endKeyboard();}
+  else if (analogRead2 < 65) {activateKeyboard(); Keyboard.press('u'); endKeyboard();}
+  else if (analogRead2 < 75) {activateKeyboard(); Keyboard.press('t'); endKeyboard();}
+  else if (analogRead2 < 85) {activateKeyboard(); Keyboard.press('s'); endKeyboard();}
+  else if (analogRead2 < 95) {activateKeyboard(); Keyboard.press('r'); endKeyboard();}
+  else if (analogRead2 < 110) {activateKeyboard(); Keyboard.press('q'); endKeyboard();}
+  else if (analogRead2 < 120) {activateKeyboard(); Keyboard.press('p'); endKeyboard();}
+  else if (analogRead2 < 140) {activateKeyboard(); Keyboard.print('o'); endKeyboard();}
+  else if (analogRead2 < 170) {activateKeyboard(); Keyboard.print('n'); endKeyboard();}
+  else if (analogRead2 < 200) {activateKeyboard(); Keyboard.print('0'); endKeyboard();}
+  else if (analogRead2 < 250) {activateKeyboard(); Keyboard.print('l'); endKeyboard();}
+  else if (analogRead2 < 350) {activateKeyboard(); Keyboard.print('j'); endKeyboard();}
+  else if (analogRead2 < 600) {activateKeyboard(); Keyboard.print('i'); endKeyboard();}
+  else {activateKeyboard(); Keyboard.print('h'); endKeyboard();}
   delay(150);             
 
 }
@@ -58,11 +58,8 @@ void loop() {
 
 void activateKeyboard () {
      Keyboard.press(KEY_LEFT_SHIFT);
-     Keyboard.press('b'); 
-     Keyboard.press('r');
-     Keyboard.press('d');
-     Keyboard.press('v'); 
-     Keyboard.press('k');  
+     Keyboard.print("brdvknrx");
+
 }
 
 void endKeyboard () {
